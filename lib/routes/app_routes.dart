02913e1a641e4ce5ad4229728b2fe6aa30c8b13a
Nuhan <nuhan@bdCallings-Mac-mini-70.local>
2025-08-20 12:27:49 +0600

    LOad: bottom nav bar

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,12 +1,16 @@
 import 'package:get/get.dart';
 import 'package:shalana07/features/auth/presentation/views/login_screen.dart';
+import '../features/bottom_nav_bar/presentation/screens/app_bottom_nav_bar.dart';
 
 class AppRoute {
   static String loginScreen = "/loginScreen";
+  static String appBottomNavBarScreen = "/appBottomNavBarScreen";
 
   static String getLoginScreen() => loginScreen;
+  static String getAppBottomNavBarScreen() => appBottomNavBarScreen;
 
   static List<GetPage> routes = [
     GetPage(name: loginScreen, page: () => const LoginScreen()),
+    GetPage(name: appBottomNavBarScreen, page: () => const AppBottomNavBar()),
   ];
 }
