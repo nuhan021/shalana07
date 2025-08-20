80d7ba7b7dadd0ddf7a5f919fe46cf30c71fce64
hrikhan <hridoykhan6786@gmail.com>
2025-08-20 09:35:27 +0600

    added:all icon

--- lib/core/utils/constants/icon_path.dart
+++ lib/core/utils/constants/icon_path.dart
@@ -2,4 +2,36 @@
 class IconPath{
   IconPath._();
   static const  String logo='';
+  static const  String  addIcon='assets/icons/add_icon.png';
+  static const  String  allRightIcon='assets/icons/alright_icon.png';
+  static const  String  arrowForwardIcon='assets/icons/arrow_forword.png';
+  static const  String  arrowBackIcon='assets/icons/back_arrow.png';
+  static const  String  iosArrowDownIcon='assets/icons/back_ios_icon.png';
+  static const  String  calanderIcon='assets/icons/calander_icon.png';
+  static const  String  copyIcon='assets/icons/copy_icon.png';
+  static const  String  deleteIcon='assets/icons/delete_icon.png';
+  static const  String  dropDownIcon='assets/icons/dropdown_and_up.png';
+  static const  String  earnCoinIcon='assets/icons/earn_coin_icon.png';
+  static const  String  editIcon ='assets/icons/edit_icon (2).png';
+  static const  String  eyeIcon ='assets/icons/eye_icon.png';
+  static const  String  favouriteIcon ='assets/icons/favourite_icon.png';
+  static const  String  filterIcon ='assets/icons/filter.png';
+  static const  String  gallaryIcon ='assets/icons/gallary_icon.png';
+  static const  String  lightIcon ='assets/icons/light_icon.png';
+  static const  String  massageSentIcon ='assets/icons/massage_sent_icon.png';
+  static const  String  navbarStoreICon ='assets/icons/nababr_store.png';
+  static const  String  navbarAddICon ='assets/icons/navbar_adding.png';
+  static const  String  navbarHomeICon ='assets/icons/navbar_home.png';
+  static const  String  navbarProfileICon ='assets/icons/navbar_profile.png';
+  static const  String  navbarGoalICon ='assets/icons/navbar_search.png';
+  static const  String  notificationIcon ='assets/icons/notification_icon.png';
+  static const  String  profileAvatarIcon ='assets/icons/profile_avater_icons.png';
+  static const  String  removeIcon ='assets/icons/remove_icon.png';
+  static const  String  timerIcon ='assets/icons/timer_icon.png';
+  static const  String  uppArrowIcon ='assets/icons/upp_arrow.png';
+  static const  String  voiceIcon ='assets/icons/voice_icon.png';
+
+
+
+
 }
