4f56aac4e6dd5daa35296da3de843bba54648a46
SaburNayem <157599779+sajalAppdevs@users.noreply.github.com>
2025-12-09 12:27:55 +0600

    custom app bar navigation and clip react image add

--- lib/core/common/widgets/custom_child_app_bar.dart
+++ lib/core/common/widgets/custom_child_app_bar.dart
@@ -1,8 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:shalana07/core/utils/helpers/app_helper.dart';
-import 'package:shalana07/features/home/child/presentation/view/child_home_screen.dart';
 import 'package:shalana07/features/notification/child/presentation/view/child_notification_page.dart';
 import 'package:shalana07/features/profile/child/presentation/view/child_profile.dart';
 
@@ -19,6 +20,7 @@ class CustomChildAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.isAvatarVisible = true,
     this.isAppBarTransparent = false,
     this.isCenterTitle = true,
+    this.image,
   });
 
   final String title;
@@ -26,6 +28,7 @@ class CustomChildAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool isAvatarVisible;
   final bool isAppBarTransparent;
   final bool isCenterTitle;
+  final String? image;
 
   @override
   Widget build(BuildContext context) {
@@ -54,15 +57,29 @@ class CustomChildAppBar extends StatelessWidget implements PreferredSizeWidget {
       actions: [
         InkWell(
           onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ChildNotificationPage()),
-            );
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(builder: (context) => ChildNotificationPage()),
+            // );
+            Get.to(() => ChildNotificationPage());
           },
-          child: Image.asset(
-            IconPath.notificationIcon,
-            height: 32.h,
-            color: AppColors.grey900,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: CachedNetworkImage(
+              imageUrl:
+                  image ??
+                  "https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper-thumbnail.png",
+              width: 34.w,
+              height: 34.w,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => Center(
+                child: LoadingAnimationWidget.staggeredDotsWave(
+                  color: AppColors.primary,
+                  size: 25.h,
+                ),
+              ),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
           ),
         ),
         SizedBox(width: 10.w),
